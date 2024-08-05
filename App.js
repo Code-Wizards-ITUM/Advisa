046aa9558d0911f1a71f714e5d4c2d60c49a8779
TharinduLala <tharindulalantha2001@gmail.com>
2024-08-05 07:24:41 +0530

    convert to js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text>Welcome to Advisa...</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
